e17ec22b1f130cc40c321a952912dadf949fec6b
AnkeshRajput <singhankesh446@gmail.com>
2025-07-02 16:10:51 +0530

    Implemented auth and storage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
+import { isAuthenticated } from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAthenticated = await isAuthenticated();
+  if(!isUserAthenticated) redirect("/signin");
+
   return (
     <div className="root-layout">
       <nav>
